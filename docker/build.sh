b2d05464bce317b5b9467381ba1987d8b5929d76
gladdiologist <1492427+gladdiologist@users.noreply.github.com>
2018-11-16 14:06:23 +0100

    enhancement

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+wait() {
+	echo "Testing service!"
+	PORT = $1
+	while true
+	do
+	  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:$PORT/status)
+	  if [ $STATUS -eq 200 ]; then
+		echo "Got 200! All done!"
+		break
+	  else
+		echo "Got $STATUS :( Not done yet..."
+	  fi
+	  sleep 10
+	done
+}
+
 docker stack rm helloan-publication
 docker stack rm helloan-seance
 docker stack rm helloan-sync
@@ -20,17 +37,4 @@ wait 9002
 
 docker stack deploy -c publication.yml helloan-publication
 
-wait() {
-	PORT = $1
-	while true
-	do
-	  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:$PORT/status)
-	  if [ $STATUS -eq 200 ]; then
-		echo "Got 200! All done!"
-		break
-	  else
-		echo "Got $STATUS :( Not done yet..."
-	  fi
-	  sleep 10
-	done
-}
+
