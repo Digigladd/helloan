07b28343c0d32378092779f1e37ba5312d43e21d
gladdiologist <1492427+gladdiologist@users.noreply.github.com>
2018-12-06 14:10:02 +0100

    erase only all non pritable unicode characters

--- helloan-utils/src/main/java/com/digigladd/helloan/utils/ArchiveParser.java
+++ helloan-utils/src/main/java/com/digigladd/helloan/utils/ArchiveParser.java
@@ -538,10 +538,10 @@ public class ArchiveParser {
 	private static String cleanTextContent(String text)
 	{
 		// strips off all non-ASCII characters
-		text = text.replaceAll("[^\\x00-\\x7F]", "");
+		//text = text.replaceAll("[^\\x00-\\x7F]", "");
 		
 		// erases all the ASCII control characters
-		text = text.replaceAll("[\\p{Cntrl}&&[^\r\n\t]]", "");
+		//text = text.replaceAll("[\\p{Cntrl}&&[^\r\n\t]]", "");
 		
 		// removes non-printable characters from Unicode
 		text = text.replaceAll("\\p{C}", "");
